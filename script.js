7e8fc49fb4a3db19ff13b23361a0500e552c9031
marianosaban <75099959+marianosaban@users.noreply.github.com>
2021-03-16 00:29:38 -0300

    ajustes

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function imprimirResultado(){
-    var x = document.getElementById("RuedaDel");
-    var i = x.selectedIndex;
-    document.getElementById("resultado").innerHTML = x.options[i].text;
+    var t = document.getElementById("transmision");
+    var i = t.selectedIndex;
+    var d = document.getElementById("RuedaDel")
+    var rd = d.selectedIndex;
+    var tr = document.getElementById("RuedaTras")
+    var rt =tr.selectedIndex
+    document.getElementById("resultado").innerHTML =t.options[i].text+" con una rueda delantera "+d.options[rd].text+" y una rueda trasera "+tr.options[rt].text;
 }
